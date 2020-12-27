94dd91a9fbafc694c2067a3a5af4fc18955229c4
rishi8900 <71644328+rishi8900@users.noreply.github.com>
2020-12-27 23:56:23 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,64 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from flask import Flask, render_template, request, redirect, url_for, session
+
+
+app = Flask(__name__)
+app.secret_key = 'any_random_string'
+
+
+@app.route('/')
+def home():
+    return render_template('home.html', user_name="General Kenobi")
+
+
+# ------------ Account Management ------------ #
+@app.route("/staff_profile")
+def staff_profile():
+    return render_template('AM/staff_profile.html')
+
+
+@app.route("/account_management")
+def account_management():
+    return render_template('AM/unlock_delete_acc.html')
+
+
+# ------------ Transaction Processing ------------ #
+
+@app.route("/incoming_items")
+def incoming_item():
+    return render_template('TP/incoming_item.html')
+
+
+# ------------ Customer Support ------------ #
+
+@app.route("/forum_FAQ")
+def forum_FAQ():
+    return render_template('CS/forum_FAQ.html')
+
+
+# ------------ Report Generation ------------ #
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template('RG/dashboard.html')
+
+
+@app.route("/cost_analysis")
+def cost_analysis():
+    return render_template('RG/cost_analysis.html')
+
+
+@app.route("/upload_insert_data")
+def upload_insert_data():
+    return render_template('RG/upload_insert_data.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/base.js
@@ -0,0 +1,11 @@
+function openNav() {
+document.getElementById("mySidenav").style.width = "16.875em";
+document.getElementById("main").style.marginLeft = "16.875em";
+document.getElementById("Main").style.marginLeft = "17.5em";
+}
+
+function closeNav() {
+document.getElementById("mySidenav").style.width = "0";
+document.getElementById("main").style.marginLeft = "0";
+document.getElementById("Main").style.marginLeft = "0.8em";
+}
