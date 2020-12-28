7b94b3982c1c73f8777b91dfc569fb79da732dd7
rishi8900 <rishi.kanneganti@gmail.com>
2020-12-28 23:52:51 +0800

    "many many"  changes
    toggle sidebar enhanced, some dash for TP staff side

--- static/base.js
+++ static/base.js
@@ -9,3 +9,16 @@ document.getElementById("mySidenav").style.width = "0";
 document.getElementById("main").style.marginLeft = "0";
 document.getElementById("Main").style.marginLeft = "0.8em";
 }
+
+var timesClicked = 0;
+
+$("#sidebarToggle").click(function() {
+timesClicked++;
+
+if (timesClicked%2==0) {
+  closeNav()
+} else {
+  openNav()
+}
+
+})
