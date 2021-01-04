78479ad8077455d633d91a00d2994d10230a9575
rishi8900 <rishi.kanneganti@gmail.com>
2021-01-04 14:27:54 +0800

    changes

--- data.py
+++ data.py
@@ -3,42 +3,60 @@ import csv
 from collections import defaultdict
 
 
-# class Data:
-#     import_csv = 0
-#
-#     def __init__(self, data_field, year, month, value):
-#         Data.import_csv += 1
-#         self.__data_field = data_field
-#         self.__year = year
-#         self.__month = month
-#         self.__value = value
-#
-#     # Accessor
-#     def get_data_field(self):
-#         return self.__data_field
-#
-#     def get_year(self):
-#         return self.__year
-#
-#     def get_month(self):
-#         return self.__month
-#
-#     def get_value(self):
-#         return self.__value
-#
-#     # Mutator Methods
-#     def set_data_field(self, data_field):
-#         self.__data_field = data_field
-#
-#     def set_year(self, year):
-#         self.__year = year
-#
-#     def set_month(self, month):
-#         self.__month = month
-#
-#     def set_value(self, value):
-#         self.__value = value
+class Data:
 
-df = pd.read_csv("costs.csv")
-saved_column = df["Campaign Costs"]
-print(saved_column)
+    def __init__(self, data_field):
+        self.__data_field = data_field
+        self.__year = ""
+        self.__month = ""
+        self.__value = 0
+
+    # Accessor
+    def get_data_field(self):
+        return self.__data_field
+
+    def get_year(self):
+        return self.__year
+
+    def get_month(self):
+        return self.__month
+
+    def get_value(self):
+        return self.__value
+
+    # Mutator Methods
+    def set_data_field(self, data_field):
+        self.__data_field = data_field
+
+    def set_year(self, year):
+        self.__year = year
+
+    def set_month(self, month):
+        self.__month = month
+
+    def set_value(self, value):
+        self.__value = value
+
+
+def read_csv():
+    many_data = []
+    try:
+        df = pd.read_csv("costs.csv", index_col = "YYYY-MM")
+        df_dict = df.to_dict()
+        print(df_dict)
+        for key in df_dict:
+            for year_month,d_value in df_dict[key].items():
+                data_info = Data(key)
+                data_info.set_year(year_month[0:4])
+                data_info.set_month(year_month[5:8])
+                data_info.set_value(d_value)
+                many_data.append(data_info)
+
+    except FileNotFoundError:
+        print("File not Found!")
+
+    for s in many_data:
+        print("{}, {}, {}, {}".format(s.get_data_field(), s.get_year(), s.get_month(), s.get_value()))
+
+
+read_csv()
